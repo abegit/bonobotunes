8a0d0b902dcf8f72a81d3310506afa19ec99cef0
abraham <abe@unscene.us>
2014-12-17 00:21:03 -0800

    rebased items

--- wpbonoboville.sh
+++ wpbonoboville.sh
@@ -1,3 +1,3 @@
-cd /Volumes/KINGSTON/dsbase/wpbonobos/public/wp-content/themes/fullby/
+cd /Volumes/KINGSTON/dsbase/wpbonobos/wp-content/themes/fullby/
 open . -a Sublime\ Text\ 2
 sass _style.scss:style.css --sourcemap --watch

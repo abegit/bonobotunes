5ab6ac97f424cb383f1fdb068e929c37c12d0451
abraham <abe@unscene.us>
2015-04-09 03:50:01 -0700

    no message

--- wp-content/plugins/ios-featured-post-slider/ios-featured-post-slider.php
+++ wp-content/plugins/ios-featured-post-slider/ios-featured-post-slider.php
@@ -30,79 +30,151 @@ class iosSlider extends SanityPluginFramework {
 	*/
 	function __construct() {
       parent::__construct(__FILE__);
+       /*
+		* #1 - Create Post Type
+		*/
+		function iosPostType() {
+		// Set UI labels
+			$labels = array(
+				'name'                => _x( 'Sliders', 'Post Type General Name', 'twentythirteen' ),
+				'singular_name'       => _x( 'Slider', 'Post Type Singular Name', 'twentythirteen' ),
+				'menu_name'           => __( 'Sliders', 'twentythirteen' ),
+				'parent_item_colon'   => __( 'Parent Slider', 'twentythirteen' ),
+				'all_items'           => __( 'All Sliders', 'twentythirteen' ),
+				'view_item'           => __( 'View Slider', 'twentythirteen' ),
+				'add_new_item'        => __( 'Add New Slider', 'twentythirteen' ),
+				'add_new'             => __( 'Add New', 'twentythirteen' ),
+				'edit_item'           => __( 'Edit Slider', 'twentythirteen' ),
+				'update_item'         => __( 'Update Slider', 'twentythirteen' ),
+				'search_items'        => __( 'Search Slider', 'twentythirteen' ),
+				'not_found'           => __( 'Not Found', 'twentythirteen' ),
+				'not_found_in_trash'  => __( 'Not found in Trash', 'twentythirteen' ),
+			);
 
-      // STEP #1 -- create options page
+		// #1.1 - technical options
+			$args = array(
+				'label'               => __( 'sliders', 'twentythirteen' ),
+				'description'         => __( 'Slider news and reviews', 'twentythirteen' ),
+				'labels'              => $labels,
+				// Features this CPT supports in Post Editor
+				'supports'            => array( 'title', 'excerpt', 'author', 'thumbnail', 'custom-fields' ),
+				// You can associate this CPT with a taxonomy or custom taxonomy. 
+				'taxonomies'          => array( 'genres' ),
+				/* A hierarchical CPT is like Pages and can have
+				* Parent and child items. A non-hierarchical CPT
+				* is like Posts.
+				*/	
+				'hierarchical'        => false,
+				'public'              => true,
+				'show_ui'             => true,
+				'show_in_menu'        => true,
+				'show_in_nav_menus'   => true,
+				'show_in_admin_bar'   => true,
+				'menu_position'       => 5,
+				'can_export'          => true,
+				'has_archive'         => true,
+				'exclude_from_search' => false,
+				'publicly_queryable'  => true,
+				'capability_type'     => 'page',
+			);
+			// Registering your Custom Post Type
+			register_post_type( 'sliders', $args );
+
+		}
+		// #1.1 initialize iosPostType
+		add_action( 'init', 'iosPostType', 0 );
+
+		// #2 - create meta boxes
+		add_action( 'add_meta_boxes', 'meta_box_post' );
+		function meta_box_post( $post ) {
+		    add_meta_box(
+		            'sliderUrl', // ID, should be a string
+		            'Slider URL', // Meta Box Title
+		            'meta_box_post_content', // Your call back function, this is where your form field will go
+		            'sliders', // The post type you want this to show up on, can be post, page, or custom post type
+		            'normal', // The placement of your meta box, can be normal or side
+		            'high' // The priority in which this will be displayed
+		        );
+		}
+		// #2.1 - Content for the custom meta box
+		function meta_box_post_content() {
+			// info current post
+		    global $post;		    
+		    //metabox value if is saved
+		    $sliderUrl = get_post_meta($post->ID, 'sliderUrl', true);
+		    // ADD here more custom field 	    
+		    // security check
+		    wp_nonce_field(__FILE__, 'ios_nonce');
+		    ?>
+		    <p>URL you want the slider to point to. <br/><input name="sliderUrl" id="sliderUrl" value="<?php echo $sliderUrl; ?>" style="border: 1px solid #ccc; margin: 10px 10px 0 0"/> <small>Example: ---> <strong>http://bonoboville.com</strong></small></p>
+		    <!-- *** ADD here more custom field  *** -->	    
+		    <?php
+			
+		}
+		// #2.2 - save function only when save
+		add_action('save_post', 'iosSaveResource');
+		function iosSaveResource(){
+		    global $post;
+		    if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) {
+		        return;
+		    }
+		    if ($_POST && wp_verify_nonce($_POST['ios_nonce'], __FILE__) ) {
+		        if ( isset($_POST['sliderUrl']) ) {
+		            update_post_meta($post->ID, 'sliderUrl', $_POST['sliderUrl']);
+		            //ADD here more custom field 
+		        }
+		    }  
+		}
+
+      // #3 - Create Options Page
 		add_action( 'admin_menu', 'iosCreatePage' );
 		function iosCreatePage(){
 			global $iosPage;
 			$file = dirname(__FILE__) . '/ios-podcast-creator/';
 			$plugin_url = plugin_dir_url($file);
 			$iosPage = add_menu_page( 'iosSlider', 'iosSlider', 'manage_options', 'iosSlider', 'iosConfigPage', $plugin_url . 'templates/assets/images/icon-backend.png', 61);
-			// add_action( 'admin_init', 'iosRegisterSettings' );
+			add_action( 'admin_init', 'iosRegisterSettings' );
 		}
-		
-		// function iosRegisterSettings() {
-		// 	register_setting( 'iosFeedStart', 'iosFeedSync');
-		// 	register_setting( 'iosFeedStart', 'iosAuthorName');
-		// 	register_setting( 'iosFeedStart', 'iosAuthorEmail');
-		// 	register_setting( 'iosFeedStart', 'iosPodcastTitle');
-		// 	register_setting( 'iosFeedStart', 'iosPodcastSummary');
-		// 	register_setting( 'iosFeedStart', 'iosPodcastImage');
-		// 	register_setting( 'iosFeedStart', 'iosExplicit');
-		// 	register_setting( 'iosFeedStart', 'iosCategories');
-		// 	register_setting( 'iosFeedStart', 'UnsceneMusicPlayer');
-		// 	register_setting( 'iosFeedStart', 'UnsceneMusicLogo');
-		// }
+		// #3.1 - Register Options
+		function iosRegisterSettings() {
+			register_setting( 'iosStart', 'iosEnablejQuery');
+			// register_setting( 'iosStart', 'iosAuthorName');
+			// register_setting( 'iosStart', 'iosAuthorEmail');
+			// register_setting( 'iosStart', 'iosPodcastTitle');
+			// register_setting( 'iosStart', 'iosPodcastSummary');
+			// register_setting( 'iosStart', 'iosPodcastImage');
+			// register_setting( 'iosStart', 'iosExplicit');
+			// register_setting( 'iosStart', 'iosCategories');
+			// register_setting( 'iosStart', 'UnsceneMusicPlayer');
+			// register_setting( 'iosStart', 'UnsceneMusicLogo');
+		}
 
 		function iosConfigPage(){
 			require_once($plugin_path.'templates/admin/page.php');
 		}
-		// start iosSlider
+
+
+		// #4 - Define Image Sizes
 		if ( function_exists( 'add_image_size' ) ) {
 			add_image_size( 'iosSlider-image', 650, 290, true );
 		}	
-		add_action("admin_init", "selection_meta_box");
-		function selection_meta_box(){
-			//add_meta_box("featured-post", "Set Featured", "featured_post", "post", "side", "low"); Uncomment this line and comment the below foreach loop if you want the Set Featured option only for posts
-			$post_types_array = get_post_types();
-			foreach ( $post_types_array as $post_type ) {
-				add_meta_box("featured-post", "Set Featured", "featured_post", $post_type, "side", "low");
-			}
-		}
-		function featured_post(){
-			global $post;
-			$meta_data = get_post_custom($post->ID);
-			$featured_post = $meta_data["_featured_post"][0];
-			$selected = ($meta_data["_featured_post"][0] == "yes") ? 'checked' : '';
-			echo "<p>";
-			echo "<input $selected type='checkbox' name='featured_post' value='yes' />";
-			echo "<label>Select this post as Featured.</label>";
-			echo "</p>";
-		}
 		
-		add_action('save_post', 'save_post_details');
 		
-		function save_post_details(){
-			global $post;
-			if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
-	    	return;
-			$featured_post = trim($_POST["featured_post"]);
-			update_post_meta($post->ID, "_featured_post", $featured_post);
-		}
+		
 
-		function show_featured_posts($tag,$numbers) {
+		function show_featured_posts($category,$numbers) {
 			global $post;
 			//get $numbers of featured posts
-			$featured_posts_array = get_posts( 'post_type=any&orderby=rand&tag='.$tag.'&numberposts='.$numbers.'&post_status=publish');
+			$featured_posts_array = get_posts( 'post_type=any&orderby=rand&category_name='.$category.'&numberposts='.$numbers.'&post_status=publish');
 			
 			$output .= '<div id="featMenu"><span class="btn sliderNav btn-primary next" style="cursor: pointer;"><i class="icon-backward2"></i></span>';
 	        $output .= '<div class="row featured loading menu"> <div class="slider">';
 
 			foreach ($featured_posts_array as $post) :  setup_postdata($post); 
-			$nivo_title = "nivo".get_the_ID(); //assign the postID as title of the image
+			$slider_title = "nivo".get_the_ID(); //assign the postID as title of the image
 				$output .= "<div class='item'>";
 				if ( function_exists("has_post_thumbnail") && has_post_thumbnail() ) { 
-				$iosImage = get_the_post_thumbnail(get_the_ID(), array(650,290), array( "class" => "post_thumbnail", 'title' => $nivo_title )); }
+				$iosImage = get_the_post_thumbnail(get_the_ID(), array(650,290), array( "class" => "post_thumbnail", 'title' => $slider_title )); }
 				$output .= $iosImage."<div id='nivo".get_the_ID()."' class='nivo-html-caption'>";
 				$output .= "<h2><a href='".get_the_permalink()."'>".get_the_title()."</a></h2>".get_the_excerpt()."</div></div>";
 
@@ -117,18 +189,12 @@ class iosSlider extends SanityPluginFramework {
 	}
 
 
-		function include_ios_scripts() {
-			wp_deregister_script( 'jquery' );
-			wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js');
-			wp_enqueue_script( 'jquery' );
-		}    
-		 
-		add_action('wp_enqueue_scripts', 'include_ios_scripts');
+		
 		
 		add_action('wp_print_styles', 'add_ios_stylesheets');
 		function add_ios_stylesheets() {
-			 wp_register_style('ios_theme_style', get_bloginfo('template_directory').'/themes/default/default.css');
-			 wp_register_style('ios_main_style', get_bloginfo('template_directory').'/js/ios-slider.css');
+			 wp_register_style('ios_theme_style', $plugin_path.'/themes/default/default.css');
+			 wp_register_style('ios_main_style', $plugin_path.'/js/ios-slider.css');
 	         wp_enqueue_style( 'ios_theme_style');
 			 wp_enqueue_style( 'ios_main_style');
 	    }
@@ -137,55 +203,54 @@ class iosSlider extends SanityPluginFramework {
 		function ios_functioncall() {
 		echo '<script src="'.plugins_url( 'ios-featured-post-slider/templates/assets/js/ioss.js', dirname(__FILE__) ).'"></script>';
 		echo '<script> var $sliderInit = jQuery.noConflict();
-$sliderInit(window).load(function() {
-var arrayOfImages = new Array();
-var bufferDistance = 0;	
-	$sliderInit(".row.featured").iosSlider({
-		desktopClickDrag: true,
-        snapToChildren: true,
-        keyboardControls: true,                
-        onSlideComplete: slideComplete,
-        onSliderLoaded: showMySlider,
+				$sliderInit(window).load(function() {
+				var arrayOfImages = new Array();
+				var bufferDistance = 0;	
+					$sliderInit(".row.featured").iosSlider({
+						desktopClickDrag: true,
+				        snapToChildren: true,
+				        keyboardControls: true,                
+				        onSlideComplete: slideComplete,
+				        onSliderLoaded: showMySlider,
 
-		autoSlide: false,
-		autoSlideTimer: 4000,
-		autoSlideTransTimer: 2000,
-		autoSlideHoverPause: true,
-		infiniteSlider: true,
+						autoSlide: false,
+						autoSlideTimer: 4000,
+						autoSlideTransTimer: 2000,
+						autoSlideHoverPause: true,
+						infiniteSlider: true,
 
-		navNextSelector: "span.prev",
-		navPrevSelector: "span.next",
-		onSlideChange: getSliderNumba
-	});
-});
+						navNextSelector: "span.prev",
+						navPrevSelector: "span.next",
+						onSlideChange: getSliderNumba
+					});
+				});
 
 
-    function getSliderNumba() {
-        $sliderInit(".item").addClass("ready");
-    }
+			    function getSliderNumba() {
+			        $sliderInit(".item").addClass("ready");
+			    }
 
-    function showMySlider() {
-        $sliderInit(".item").addClass("ready");
-    }
+			    function showMySlider() {
+			        $sliderInit(".item").addClass("ready");
+			    }
 
-    function slideComplete(args) {
-        	$sliderInit("span.next, span.prev").removeClass("unselectable");
-        if(args.currentSlideNumber == 1) {
-            $sliderInit("span.prev").addClass("unselectable");
-        } else if(args.currentSliderOffset == args.data.sliderMax) {
-            $sliderInit("span.next").addClass("unselectable");
- 	   }
-	}
-</script>';
-		}
+			    function slideComplete(args) {
+			        	$sliderInit("span.next, span.prev").removeClass("unselectable");
+			        if(args.currentSlideNumber == 1) {
+			            $sliderInit("span.prev").addClass("unselectable");
+			        } else if(args.currentSliderOffset == args.data.sliderMax) {
+			            $sliderInit("span.next").addClass("unselectable");
+			 	   }
+				}
+			</script>';}
 		
 		function tg_featured_posts($atts, $content = null) {
 			ob_start();
 			extract(shortcode_atts(array(
 				"numbers" => '5',
-				"tag" => 'featured'
+				"category" => 'featured'
 			), $atts));
-			echo show_featured_posts($tag,$numbers);
+			echo show_featured_posts($category,$numbers);
 		 	$result = ob_get_contents(); // get everything in to $result variable
 		    ob_end_clean();
 		    return $result;
@@ -197,18 +262,14 @@ var bufferDistance = 0;
 
 
 		// misc
-		if ( get_option('iosFeedSync') == '1') { 
-				add_action( 'after_setup_theme', 'addCustomPodcastRSS' );
-				function addCustomPodcastRSS() {
-					add_feed( 'listen', 'iosPodcast' );
-					add_feed( 'ios', 'iosiOS' );
-					function iosPodcast() {
-						require_once($plugin_path.'templates/podcast/ios.php');
-					}
-					function iosiOS() {
-						require_once($plugin_path.'templates/podcast/iosSlider.php');
-					}
-				}
+		if ( get_option('iosEnablejQuery') == '1') { 
+			function include_ios_scripts() {
+				wp_deregister_script( 'jquery' );
+				wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js');
+				wp_enqueue_script( 'jquery' );
+			}    
+			 
+			add_action('wp_enqueue_scripts', 'include_ios_scripts');
 		}
 
 
@@ -217,34 +278,19 @@ var bufferDistance = 0;
 	*		Run during the activation of the plugin
 	*/
 	function activated() {
-		// if (get_option('iosAuthorEmail') == '') {
-		// 	$iosleader = get_option('admin_email');
-		// } else {
-
-		// }
-		
-		// add_option("iosFeedSync", '1', '', 'yes');
-		// add_option("iosAuthorName", 'Mr. Unscene', '', 'yes');
-		// add_option("iosAuthorEmail", '', '', 'yes');
-		// add_option("iosPodcastTitle", '', '', 'yes');
-		// add_option("iosPodcastSummary", '', '', 'yes');
-		// add_option("iosPodcastImage", '', '', 'yes');
-		// add_option("iosExplicit", '1', '', 'yes');
-		// add_option("iosCategories", 'Design', '', 'yes');
-		// add_option("UnsceneMusicPlayer", '1', '', 'yes');	
-		// add_option("UnsceneMusicLogo", '', '', 'yes');
+		add_option("iosEnablejQuery", '1', '', 'yes');
 	}
 	/*
 	*		Run during the initialization of Wordpress
 	*/
 	function initialized() {
-		
+
 	}
 	
 	// abedit
-    
 	    var $admin_css = array('style' , 'fonts');
 		// var $admin_js = array('script');
+
 }
 
 
